49676e5845ac823c19e978d850be0ef2d6ba5e1c
Vijayaraj <vjlingam7@gmail.com>
2024-11-01 03:53:35 +0530

    Added pattern8

--- Patterns-Loops/Patterns.py
+++ Patterns-Loops/Patterns.py
@@ -49,6 +49,18 @@ def pattern7(n):
             print(" ",end=" ")
         print()
 
-pattern7(3)
+def pattern8(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ", end=" ")
+
+        for j in range(((2 * n) - (2 * i)) - 1):
+            print("*", end=" ")
+
+        for j in range(i):
+            print(" ", end=" ")
+        print()
+
+pattern8(3)
 print()
-pattern7(5)
+pattern8(5)
