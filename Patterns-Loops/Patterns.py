0cf1f285cfca6d834466c1f3f403d04e6d4747ff
Vijayaraj <vjlingam7@gmail.com>
2024-11-01 03:07:29 +0530

    Added patterns 2,3 & 4....

--- Patterns-Loops/Patterns.py
+++ Patterns-Loops/Patterns.py
@@ -4,6 +4,24 @@ def pattern1(n):
             print("*",end="")
         print()
 
-pattern1(3)
+def pattern2(n):
+    for i in range(n):
+        for j in range(i + 1):
+            print("*", end=" ")
+        print()
+
+def pattern3(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+def pattern4(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(i, end=" ")
+        print()
+
+pattern4(3)
 print()
-pattern1(4)
+pattern4(5)
