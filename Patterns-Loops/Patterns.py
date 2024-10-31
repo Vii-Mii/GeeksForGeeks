a3689bef27714e4235cd3a1ae8e9a830c8970e90
Vijayaraj <vjlingam7@gmail.com>
2024-11-01 04:40:11 +0530

    Added pattern12

--- Patterns-Loops/Patterns.py
+++ Patterns-Loops/Patterns.py
@@ -87,6 +87,18 @@ def pattern11(n):
             val = 1 - val
         print()
 
-pattern11(3)
+def pattern12(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+
+        for j in range((2 * n) - (2 * i)):
+            print(" ", end=" ")
+
+        for j in range(i, 0, -1):
+            print(j, end=" ")
+        print()
+
+pattern12(3)
 print()
-pattern11(5)
+pattern12(5)
