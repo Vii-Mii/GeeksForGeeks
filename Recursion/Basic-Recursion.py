a76b4f52832016dd3b28acd3d41bd4dfe38e0b50
Vijayaraj <vjlingam7@gmail.com>
2024-11-01 14:04:45 +0530

    Updated few more basic recursion problems

--- Recursion/Basic-Recursion.py
+++ Recursion/Basic-Recursion.py
@@ -36,6 +36,46 @@ def printNos(self, n):
     print(n, end=" ")
     self.printNos(n - 1)
 
+# sum of N numbers using recursion
+def summ(n):
+    if n == 0:
+        return 0
+    return n + summ(n-1)
+
+# factorial for N numbers using recursion]
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n-1)
+
+# Reverse an array using recursion
+def reverse(arr,n,i):
+    if i >= n//2 :
+        return
+    arr[i],arr[n-i-1] = arr[n-i-1],arr[i]
+    reverse(arr,n,i+1)
+
+
+# Palindrome using recursion
+def palindrome(str,n,i):
+    if i >= n//2:
+        return True
+    if str[i] != str[n-i-1]:
+        return False
+    return palindrome(str,n,i+1)
+
+# Fibinocci
+def fibinocci(n):
+    if n <= 1:
+        return n
+    return fibinocci(n-1) + fibinocci(n-2)
+
 if __name__ == '__main__':
-    func()
-    printing()
+    print(summ(6))
+    print(fact(6))
+    arr = [1,4,2,5,6,7]
+    reverse(arr,6,0)
+    print(arr)
+    str = "racehar"
+    print(palindrome(str,7,0))
+    print(fibinocci(5))
