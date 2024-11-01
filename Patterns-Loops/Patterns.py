53bc3e6d86e327a0a177edab7d8d21333cf0eb94
Vijayaraj <vjlingam7@gmail.com>
2024-11-01 05:42:39 +0530

    Added pattern 19 & 20

--- Patterns-Loops/Patterns.py
+++ Patterns-Loops/Patterns.py
@@ -154,6 +154,52 @@ def pattern18(n):
             print(chr(val+j),end=" ")
         print()
 
-pattern18(3)
+def pattern19(n):
+    for i in range(n):
+        for j in range(n - i):
+            print("*", end=" ")
+
+        for j in range(2 * i):
+            print(" ", end=" ")
+
+        for j in range(n - i):
+            print("*", end=" ")
+        print()
+
+    for i in range(n):
+        for j in range(i + 1):
+            print("*", end=" ")
+
+        for j in range(2 * n - (2 * i + 2)):
+            print(" ", end=" ")
+
+        for j in range(i + 1):
+            print("*", end=" ")
+        print()
+
+def pattern20(n):
+    for i in range(n-1):
+        for j in range(i + 1):
+            print("*", end=" ")
+
+        for j in range(2 * n - (2 * i + 2)):
+            print(" ", end=" ")
+
+        for j in range(i + 1):
+            print("*", end=" ")
+        print()
+
+    for i in range(n):
+        for j in range(n - i):
+            print("*", end=" ")
+
+        for j in range(2 * i):
+            print(" ", end=" ")
+
+        for j in range(n - i):
+            print("*", end=" ")
+        print()
+
+pattern20(3)
 print()
-pattern18(5)
+pattern20(5)
